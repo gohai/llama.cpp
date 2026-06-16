b468e969a43301af5dbd0f13c1b6e14b1e9ede3d
gohai <gottfried.haider@gmail.com>
2026-06-16 12:31:10 +0800

    server: Allow Private Network Access (PNA)

--- tools/server/server-http.cpp
+++ tools/server/server-http.cpp
@@ -270,6 +270,7 @@ bool server_http_context::init(const common_params & params) {
     // register server middlewares
     srv->set_pre_routing_handler([middleware_validate_api_key, middleware_server_state](const httplib::Request & req, httplib::Response & res) {
         res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin"));
+        res.set_header("Access-Control-Allow-Private-Network", "true");
         // If this is OPTIONS request, skip validation because browsers don't include Authorization header
         if (req.method == "OPTIONS") {
             res.set_header("Access-Control-Allow-Credentials", "true");
